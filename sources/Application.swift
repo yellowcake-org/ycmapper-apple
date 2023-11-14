f229d9a542cc91e80176d29576d9ca9c5e7377cb
Alexander Orlov <me@0xceed.com>
2023-11-14 21:17:15 +0300

    Blank XCode project.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

---
+++ sources/Application.swift
@@ -0,0 +1,17 @@
+//
+//  Application.swift
+//  yc-mapper
+//
+//  Created by Alexander Orlov on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct yc_mapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
