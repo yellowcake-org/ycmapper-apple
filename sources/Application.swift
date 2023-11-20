6e4be68a27f8764d2a6443c22782782849975742
Alexander Orlov <me@0xceed.com>
2023-11-20 03:36:12 +0300

    Rendering first frame.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- sources/Application.swift
+++ sources/Application.swift
@@ -12,16 +12,6 @@ struct ycmapper: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .onAppear(perform: {
-                    let status = yc_vid_view_initialize(
-                        .none,
-                        .none,
-                        .none,
-                        .none
-                    )
-
-                    debugPrint("status == \(status)")
-            })
         }
     }
 }
