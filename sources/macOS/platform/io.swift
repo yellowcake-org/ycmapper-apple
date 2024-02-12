6c1eae406bedb9383725e5ddb50e5116b91bbca1
Alexander Orlov <me@0xceed.com>
2024-02-13 02:32:30 +0300

    Fixes #13 (crash when optimizations are on).

--- sources/macOS/platform/io.swift
+++ sources/macOS/platform/io.swift
@@ -5,7 +5,7 @@
 //  Created by Alexander Orlov on 23.11.2023.
 //
 
-public let io_fs_api = yc_res_io_fs_api_t(
+public var io_fs_api = yc_res_io_fs_api_t(
     fopen: { filename, mode in .init(mutating: fopen(filename, mode)) },
     fclose: { file in file.flatMap({ fclose($0.assumingMemoryBound(to: FILE.self)) }) ?? -1 },
     fread: { dest, size, nitems, file in
